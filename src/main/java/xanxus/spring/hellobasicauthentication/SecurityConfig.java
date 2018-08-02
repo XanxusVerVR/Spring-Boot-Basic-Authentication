c2029817d4f22a63b1974a1e29d3ce2932eb9818
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2018-08-02 22:39:56 +0800

    加了session設置 第二次密碼不一樣也不能通過驗證了

--- src/main/java/xanxus/spring/hellobasicauthentication/SecurityConfig.java
+++ src/main/java/xanxus/spring/hellobasicauthentication/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -35,6 +36,8 @@ public class SecurityConfig {
                     .anyRequest().hasRole("ADMIN")
                     .and()
                     .httpBasic();
+            http.sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.NEVER);
         }
     }
 }
